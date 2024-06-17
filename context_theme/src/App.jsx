8a0058934e18e4c000a536d469c6af524bd87659
itsshivamdwivedi <88644787+itsshivamdwivedi@users.noreply.github.com>
2024-06-17 11:46:35 +0000

    commit

---
+++ context_theme/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { ThemeProvider } from './Contexts/Theme'
+
+function App() {
+
+
+  return (
+    <ThemeProvider>
+    <div className="flex flex-wrap min-h-screen items-center">
+                <div className="w-full">
+                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">                       
+                    </div>
+
+                    <div className="w-full max-w-sm mx-auto">
+                       
+                    </div>
+                </div>
+            </div>
+    </ThemeProvider>
+  )
+}
+
+export default App
---
+++ context_theme/src/Contexts/Theme.js
@@ -0,0 +1,20 @@
+import { createContext,useContext } from "react";
+
+// here we are creating a themecontext  and in that we are creating methods
+export const ThemeContext = createContext({
+    themeMode: "light",  // default theme
+    darkTheme: () => {},
+    lightTheme: () => {},
+    // the above two lines are basically a method 
+    //  WHen a user calls the context he will get the default theme and this two methods also
+
+
+})
+//  so  below line is basically a provider of above function
+//  so themeprovider is the provider of themecontext
+//  we will use it for wrapping the elements 
+export const ThemeProvider = ThemeContext.Provider
+// usually everyone make their custom hooks here and we can also export that 
+export default function useTheme(){
+    return useContext(ThemeContext)
+}
