640692e9f90f8fc08dc17e9a36593761a540a7ca
itsshivamdwivedi <88644787+itsshivamdwivedi@users.noreply.github.com>
2024-06-17 06:17:39 +0000

    commit

---
+++ Theme_Switcher/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+
+
+  return (
+    <>
+     <div className=' bg-gray-600 text-center text-3xl'>Hello I'm Shivam Dwivedi</div>
+    </>
+  )
+}
+
+export default App
