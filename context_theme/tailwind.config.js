393786f341cb4bead218d8b64716e5fc5db361ca
itsshivamdwivedi <88644787+itsshivamdwivedi@users.noreply.github.com>
2024-07-10 06:48:32 +0000

    themeswitcher

--- context_theme/tailwind.config.js
+++ context_theme/tailwind.config.js
@@ -4,6 +4,8 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  // to use dark theme we have to enable the configuration in tailwind css which is a dark mode 
+  darkMode:"class",
   theme: {
     extend: {},
   },
